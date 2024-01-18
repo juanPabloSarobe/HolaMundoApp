4765bdd6591e5ade9e38979c31cc43575ce3c07c
juanPabloSarobe <jp.sarobe@gmail.com>
2024-01-18 20:41:52 -0300

    iniciando Hola mundo app

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Hola mundo</Text>
+      <Text style={styles.title}>Estoy empezando a programar APPs </Text>
+      <Text style={styles.signature}>JP </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 30,
+  },
+  title: {
+    fontSize: 60,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  signature: {
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    fontSize: 80,
+    textAlign: "right",
+    marginTop: 20,
   },
 });
